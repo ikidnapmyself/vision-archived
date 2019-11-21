609da1ab7f595312ebac83f3932a82fd3120c1cf
Burak <burakalbayrak@live.com>
2019-11-22 00:04:12 +0300

    User Model revised

---
+++ app/Models/User.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasUUID;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable
+{
+    use HasUUID, Notifiable, SoftDeletes, HasRoles, CanResetPassword;
+
+    /**
+     * Set auto incrementing for primary key.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'surname', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * Get the user's full name.
+     *
+     * @return string
+     */
+    public function getFullNameAttribute()
+    {
+        return "{$this->name} {$this->surname}";
+    }
+
+    /**
+     * Set the user's name
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = Str::title($value);
+    }
+
+    /**
+     * Set the user's surname.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setSurnameAttribute($value)
+    {
+        $this->attributes['surname'] = Str::title($value);
+    }
+
+    /**
+     * Set the user's surname.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setEmailAttribute($value)
+    {
+        $this->attributes['email'] = Str::lower($value);
+    }
+}
