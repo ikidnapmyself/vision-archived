16ca5eae8a24efa85f429ca17c3ba72f03e1e344
Burak <burakalbayrak@live.com>
2019-11-23 13:16:12 +0300

    avoid col usage in cards

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div class="card {{ $col ?? 'col' }} {{ config('ui.shadow') ? 'shadow-sm' : '' }}">
+<div class="card {{ config('ui.shadow') ? 'shadow-sm' : '' }}">
     @if(isset($title))
     <div class="card-header">
         <h4 class="my-0 font-weight-normal">{{ $title }}</h4>
@@ -16,5 +16,20 @@
         @if(isset($button))
             <a class="btn btn-lg btn-block btn-outline-primary">{{ $button }}</a>
         @endif
+    </div>
+</div>
+<div class="card mb-4 shadow-sm">
+    <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Free</h4>
+    </div>
+    <div class="card-body">
+        <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+            <li>10 users included</li>
+            <li>2 GB of storage</li>
+            <li>Email support</li>
+            <li>Help center access</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-outline-primary">Sign up for free</button>
     </div>
 </div>
