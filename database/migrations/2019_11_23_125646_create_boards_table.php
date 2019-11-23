dc08bb704726b4df42333bb19a6760637573c304
Burak <burakalbayrak@live.com>
2019-11-23 16:03:53 +0300

    boards

---
+++ database/migrations/2019_11_23_125646_create_boards_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boards', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('name');
+            $table->string('description');
+            $table->boolean('is_public')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->primary();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boards');
+    }
+}
