f372408201e6698f21e66619a10c1a25f07e0cf5
Burak <burakalbayrak@live.com>
2019-11-23 13:06:06 +0300

    ui.dark

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,16 @@
 @extends('layouts.app')
-
 @section('content')
+<div class="jumbotron">
+    <div class="container">
+        <h1 class="display-3">Hello, world!</h1>
+        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+    </div>
+</div>
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+        @component('components.deck')
+        @endcomponent
     </div>
 </div>
 @endsection
