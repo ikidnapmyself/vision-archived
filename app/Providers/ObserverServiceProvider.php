77dfb6807ad9c288b27e8dcdf50b074f501fd08f
Burak <burakalbayrak@live.com>
2019-11-27 01:54:58 +0300

    observer provider

---
+++ app/Providers/ObserverServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Team;
+use App\Observers\TeamObserver;
+use Illuminate\Support\ServiceProvider;
+
+class ObserverServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+}
