850725490b9568f03862a3d874ea30a9168c0310
Burak <burakalbayrak@live.com>
2019-11-28 18:53:24 +0300

    default height has changed

--- resources/views/components/avatar.blade.php
+++ resources/views/components/avatar.blade.php
@@ -2,4 +2,4 @@
 <img src="{{ $user->getUrlfriendlyAvatar() }}"
      alt="{{ $user->full_name }}"
      title="{{ $user->full_name }}"
-     class="rounded-sm" style="max-height: 30px;">
+     class="rounded-sm" style="max-height: 20px;">
