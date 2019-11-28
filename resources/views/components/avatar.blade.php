726b2d28bd3bdee444bdec5f1e2f69d073330a5a
Burak <burakalbayrak@live.com>
2019-11-28 18:11:24 +0300

    avatar component

---
+++ resources/views/components/avatar.blade.php
@@ -0,0 +1,5 @@
+@php($user = $user ?? auth()->user() )
+<img src="{{ $user->getUrlfriendlyAvatar() }}"
+     alt="{{ $user->full_name }}"
+     title="{{ $user->full_name }}"
+     class="rounded-sm" style="max-height: 30px;">
