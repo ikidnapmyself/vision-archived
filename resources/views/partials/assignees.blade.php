62a474eba54801c276e903d2ea36df95582fe44b
Burak <burakalbayrak@live.com>
2019-11-28 18:46:16 +0300

    assignees partial

---
+++ resources/views/partials/assignees.blade.php
@@ -0,0 +1,35 @@
+<div class="list-group">
+    @foreach($assignees as $assignee)
+        <a class="list-group-item list-group-item-action">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">
+                    @component('components.avatar', ['user' => $assignee->user])
+                    @endcomponent
+                    {{ $assignee->user->full_name }}
+                    @if(!is_null($assignee->completed_at))
+                        <span
+                            @component('components.tooltip', ['message' => __('task.Completed On', ['date' => $assignee->completed_at])])
+                            @endcomponent
+                        >
+                            <i class="fa fa-check text-success"></i>
+                        </span>
+                    @endif
+                </h5>
+                @if(true === $assignee->blocker)
+                <small class="text-danger"><i class="fa fa-ban"></i> @lang('partials.assignees.Blocker')</small>
+                @endif
+            </div>
+            @if(!is_null($assignee->defer))
+                <small>
+                    <i class="fa fa-calendar-o"></i> @lang('partials.assignees.Deferred', ['date' => $assignee->user->defer])
+                </small>
+                <br>
+            @endif
+            @if(!is_null($assignee->user->due))
+                <small>
+                    <i class="fa fa-clock-o"></i> @lang('partials.assignees.Due', ['date' => $assignee->user->due])
+                </small>
+            @endif
+        </a>
+    @endforeach
+</div>
