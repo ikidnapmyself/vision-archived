8fdcc11d9f5769b23a0e28c98bfb2982cc2c18e6
Burak <burakalbayrak@live.com>
2019-11-28 15:37:13 +0000

    Apply fixes from StyleCI

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -1,21 +1,20 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-
 use App\Models\Project;
 use App\Models\Task;
 use Faker\Generator as Faker;
 
 $factory->define(Task::class, function (Faker $faker) {
-
     $project = Project::inRandomOrder()->first();
     $boolean = $faker->boolean;
+
     return [
         'project_id'   => $boolean ? $project->id : null,
         'name'         => $boolean ? $project->name : $faker->sentence,
         'body'         => $faker->paragraph,
-        'starred'      => $faker->boolean ?? ! (boolean) rand(0, 2),
-        'flagged'      => $faker->boolean ?? ! (boolean) rand(0, 2),
+        'starred'      => $faker->boolean ?? ! (bool) rand(0, 2),
+        'flagged'      => $faker->boolean ?? ! (bool) rand(0, 2),
         'completed_by' => null,
     ];
 });
