f0c3a919252ef7ca017c98ebd57555fd99843d12
Burak <burakalbayrak@live.com>
2019-11-29 17:13:04 +0300

    task edit view

--- resources/views/task/edit.blade.php
+++ resources/views/task/edit.blade.php
@@ -5,12 +5,28 @@
         <h3 class="title">@lang('task.edit.Edit', ['name' => $task->name])</h3>
         <div class="row">
             <div class="col-12">
-                @component('components.form', ['action' => ''])
+                @component('components.form', ['action' => route('task.update', ['task' => $task->id])])
                     @slot('form')
                         <div class="card">
                             <div class="card-body">
                                 <h5 class="card-title">{{ $task->name }}</h5>
-                                <p class="card-text">{{ $task->body }}</p>
+                                @component('components.group', [
+                                    'code'   => 'name',
+                                    'value'  => $task->name,
+                                    'lang'   => __('task.edit.name'),
+                                    'inline' => __('task.edit.name_inline'),
+                                ])
+                                @endcomponent
+                                @component('components.group', [
+                                    'input'  => 'textarea',
+                                    'code'   => 'body',
+                                    'value'  => $task->body,
+                                    'lang'   => __('task.edit.body'),
+                                    'inline' => __('task.edit.body_inline'),
+                                ])
+                                @endcomponent
+                                @component('components.forms.update')
+                                @endcomponent
                             </div>
                         </div>
                     @endslot
