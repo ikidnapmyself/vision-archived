f7e6c7c9ebed0b36e38acd55507663d85069020d
Burak <burakalbayrak@live.com>
2019-11-29 14:42:13 +0300

    service interface

---
+++ app/Interfaces/ServiceInterface.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Interfaces;
+
+interface ServiceInterface
+{
+    public function repository();
+
+    public function index();
+
+    /**
+     * Display model.
+     *
+     * @param string $id
+     * @return mixed
+     */
+    public function show(string $id);
+    public function create();
+
+    /**
+     * Update a model.
+     *
+     * @param array $attributes
+     * @param string $id
+     * @return mixed
+     */
+    public function update(array $attributes, string $id);
+    public function delete();
+}
