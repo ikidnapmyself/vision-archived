69194629d274ffe48d459d79ec30d38c6162608e
Burak <burakalbayrak@live.com>
2019-11-30 11:00:22 +0300

    Service Interface

--- app/Interfaces/ServiceInterface.php
+++ app/Interfaces/ServiceInterface.php
@@ -3,8 +3,18 @@ namespace App\Interfaces;
 
 interface ServiceInterface
 {
+    /**
+     * Get repository.
+     *
+     * @return mixed
+     */
     public function repository();
 
+    /**
+     * Paginate models.
+     *
+     * @return mixed
+     */
     public function index();
 
     /**
@@ -14,15 +24,29 @@ interface ServiceInterface
      * @return mixed
      */
     public function show(string $id);
-    public function create();
+
+    /**
+     * Utilize repository to create a model.
+     *
+     * @param array|null $attributes
+     * @return mixed
+     */
+    public function create(?array $attributes = []);
 
     /**
      * Update a model.
      *
-     * @param array $attributes
+     * @param string $id
+     * @param array|null $attributes
+     * @return mixed
+     */
+    public function update(string $id, ?array $attributes = []);
+
+    /**
+     * Delete a model.
+     *
      * @param string $id
      * @return mixed
      */
-    public function update(array $attributes, string $id);
-    public function delete();
+    public function delete(string $id);
 }
