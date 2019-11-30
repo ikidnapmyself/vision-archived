58fcce3ab143f7ceb15e4ed96a496a52d9163270
Burak <burakalbayrak@live.com>
2019-11-30 19:28:03 +0300

    task controller and delete feature

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -27,10 +27,14 @@
                             @lang('task.show.Complete')
                         </a>
                     @endif
-                    <a href="#" class="card-link text-danger">
+                    <a onclick="event.preventDefault();document.getElementById('delete-{{ $task->id }}-form').submit();" class="card-link text-danger">
                         <i class="fa fa-trash-alt"></i>
                         @lang('task.show.Delete')
                     </a>
+                    <form id="delete-{{ $task->id }}-form" action="{{ route('task.destroy', ['task' => $task->id]) }}" method="POST" style="display: none;">
+                        @method('DELETE')
+                        @csrf
+                    </form>
                 </div>
             </div>
         </div>
