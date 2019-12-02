6b262a37829ac2b34e0d5c3f76acd5f874ffcc15
Burak <burakalbayrak@live.com>
2019-12-02 23:50:28 +0300

    home is blank

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,47 +9,6 @@
     </div>
 </div>
 <div class="container">
-    <div class="row justify-content-center">
-        @component('components.deck')
-            @slot('body')
-                @component('components.card', [
-                        'title' => __('home.Hourly'),
-                        'body'  => '<i class="fa fa-clock-o"></i>'
-                    ])
-                @endcomponent
-                @component('components.card', [
-                        'title' => __('home.Daily'),
-                        'body'  => '<i class="fa fa-calendar-day"></i>'
-                    ])
-                @endcomponent
-                @component('components.card', [
-                        'title' => __('home.Weekly'),
-                        'body'  => '<i class="fa fa-calendar-alt"></i>'
-                    ])
-                @endcomponent
-            @endslot
-        @endcomponent
-    </div>
-    <div class="row justify-content-center">
-        @component('components.deck')
-            @slot('body')
-                @component('components.card', [
-                        'title' => __('home.Personal'),
-                        'body'  => '<i class="fa fa-clock-o"></i>'
-                    ])
-                @endcomponent
-                @component('components.card', [
-                        'title' => __('home.Board'),
-                        'body'  => '<i class="fa fa-calendar-day"></i>'
-                    ])
-                @endcomponent
-                @component('components.card', [
-                        'title' => __('home.Team'),
-                        'body'  => '<i class="fa fa-calendar-alt"></i>'
-                    ])
-                @endcomponent
-            @endslot
-        @endcomponent
-    </div>
+
 </div>
 @endsection
