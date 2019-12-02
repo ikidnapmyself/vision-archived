9b0fecdbe4bf6ec8232edbc71ff1f11d89df0a96
Burak <burakalbayrak@live.com>
2019-12-02 21:52:00 +0000

    Apply fixes from StyleCI

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -1,16 +1,17 @@
 <?php
+
 namespace App\Models;
 
 use Spatie\ModelStatus\Status as BaseStatus;
 
 class Status extends BaseStatus
 {
-    const BACKLOG     = 'backlog';
-    const TODO        = 'todo';
+    const BACKLOG = 'backlog';
+    const TODO = 'todo';
     const PROGRESSING = 'progressing';
-    const COMPLETED   = 'completed';
-    const CANCELED    = 'canceled';
-    const ARCHIVED    = 'archived';
-    const DELETED     = 'deleted';
-    const FAILED      = 'failed';
+    const COMPLETED = 'completed';
+    const CANCELED = 'canceled';
+    const ARCHIVED = 'archived';
+    const DELETED = 'deleted';
+    const FAILED = 'failed';
 }
