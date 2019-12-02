ce4564a58bad2f8fa7fea8de1480cdbc825b2ac1
Burak <burakalbayrak@live.com>
2019-12-02 23:47:45 +0300

    status lang lines

---
+++ resources/lang/en/status.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'backlog'     => 'Backlog',
+    'todo'        => 'To Do',
+    'progressing' => 'In Progress',
+    'completed'   => 'Completed',
+    'canceled'    => 'Canceled',
+    'archived'    => 'Archived',
+    'deleted'     => 'Deleted',
+    'failed'      => 'Failed',
+];
