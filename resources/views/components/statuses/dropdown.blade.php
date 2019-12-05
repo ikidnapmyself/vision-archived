5dbd892cbe2133b26566ead2e81d5ab27e79ea9e
Burak <burakalbayrak@live.com>
2019-12-05 19:24:46 +0300

    keep dropdown blade example

--- resources/views/components/statuses/dropdown.blade.php
+++ resources/views/components/statuses/dropdown.blade.php
@@ -1,4 +1,33 @@
 @php
     $config= config('model-status');
 @endphp
-<status-component :model="{{ $model->toJson() }}" :current="{{ $model->status()->toJson() }}" :statuses="{{ json_encode($model->availableStatuses()) }}"></status-component>
+<div class="btn-group">
+    <button class="btn btn-{{ $config['colors'][$model->status] }}"
+            type="button" id="status-{{ $model->id }}" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="{{ $config['icons'][$model->status] }}"></i>
+        @lang("status.{$model->status}")
+    </button>
+    <button type="button" class="btn btn-{{ $config['colors'][$model->status] }} dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <span class="sr-only">Toggle Dropdown</span>
+    </button>
+    <div class="dropdown-menu" aria-labelledby="status-{{ $model->id }}">
+        <b class="px-3">@lang('components.statuses.Move to')</b>
+        <div class="dropdown-divider"></div>
+        @foreach($model->availableStatuses() as $status)
+            @if($model->status !== $status)
+                @if(View::exists("components.statuses.buttons.{$status}"))
+                    @component("components.statuses.buttons.{$status}", [
+                        'model' => $model,
+                        'type'  => $type ?? 'dropdown',
+                    ])
+                    @endcomponent
+                @else
+                    <a class="dropdown-item text-{{ $config['colors'][$status] }}" href="#">
+                        <i class="{{ $config['icons'][$status] }}"></i>
+                        @lang("status.{$status}")
+                    </a>
+                @endif
+            @endif
+        @endforeach
+    </div>
+</div>
