95718239dd230c427a1392708fbd3b97d759bc87
Burak <burakalbayrak@live.com>
2019-12-21 18:24:48 +0300

    home intro

--- resources/js/components/home/HomeIntroComponent.vue
+++ resources/js/components/home/HomeIntroComponent.vue
@@ -4,7 +4,8 @@
             <h1 class="display-3">{{ $t('home.hello', {Name: user.name}) }}</h1>
             <p>{{ $t('home.Intro') }}</p>
             <p>
-                <b-link class="btn btn-primary btn-lg" href="#" role="button">
+                <b-link class="btn btn-primary btn-lg" href="task/create" role="button">
+                    <i class="fa fa-thumbtack"></i>
                     {{ $t('home.Create Task') }}
                 </b-link>
             </p>
