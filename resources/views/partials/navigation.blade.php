9a2b2290beaf8ecf03aeeb4a2be9356e496259e9
Burak <burakalbayrak@live.com>
2019-12-30 01:07:28 +0300

    profile link

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -37,6 +37,9 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('user.show', ['user' => Auth::user()->id]) }}">
+                                {{ Auth::user()->name }}
+                            </a>
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
