b39587c3db323f8eb571fc5d16a67d18955a4d0d
Burak <burakalbayrak@live.com>
2019-12-31 13:00:16 +0300

    Anybody is my friend

--- database/seeds/FriendshipsTableSeeder.php
+++ database/seeds/FriendshipsTableSeeder.php
@@ -12,19 +12,28 @@ class FriendshipsTableSeeder extends Seeder
      */
     public function run()
     {
+        $test     = User::where('email', 'test@test.com')->first();
+        User::all()->each(function ($user) use ($test) {
+            /**
+             * @var User $test
+             * @var User $user
+             */
+            $check = $test->befriend($user);
+            $check === true ? $test->acceptFriendRequest($user) : false;
+        });
+
         for ($count = 0; $count < 350; $count++) {
             /** @var User $sender */
             $sender    = User::inRandomOrder()->first();
             /** @var User $recipient */
             $recipient = User::inRandomOrder()->first();
 
-            if (true === $sender->isFriendWith($recipient)) {
+            if (false === $sender->canBefriend($recipient)) {
                 $recipient = factory(User::class)->create();
             }
 
             $sender->befriend($recipient);
             $sender->acceptFriendRequest($recipient);
-
         }
     }
 }
