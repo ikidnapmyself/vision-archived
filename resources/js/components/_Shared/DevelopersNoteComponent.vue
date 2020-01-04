42bdf443b38e2c7cd0449c8cb752c09887bf7ff9
Burak <burakalbayrak@live.com>
2020-01-04 17:43:36 +0300

    developers note component

---
+++ resources/js/components/_Shared/DevelopersNoteComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <span>
+        <b-link id="popover-3">
+            <i class="fa fa-comment-dots"></i>
+        </b-link>
+        <b-popover target="popover-3" triggers="hover focus">
+            <template v-slot:title>
+                {{ $t('components.task.developers-note.Developers Note') }}
+            </template>
+            {{ note }}
+        </b-popover>
+    </span>
+</template>
+
+<script>
+    export default {
+        props: ['note'],
+    }
+</script>
