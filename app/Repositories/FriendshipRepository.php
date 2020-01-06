c861ba21ca87be14958af4e98a3fe43b53233ba5
Burak <burakalbayrak@live.com>
2020-01-07 01:37:28 +0300

    friendship repository

---
+++ app/Repositories/FriendshipRepository.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\Friendship;
+
+class FriendshipRepository extends Repository
+{
+
+    /**
+     * Specify Model class name
+     *
+     * @return string
+     */
+    public function model()
+    {
+        return Friendship::class;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function index()
+    {
+        return $this->repository->paginate();
+    }
+}
