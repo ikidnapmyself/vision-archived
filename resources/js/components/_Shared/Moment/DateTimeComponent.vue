7791b5f7b5b8ebfcdc610e585246101ea4797a43
Burak <burakalbayrak@live.com>
2020-01-07 23:03:46 +0300

    format loop

--- resources/js/components/_Shared/Moment/DateTimeComponent.vue
+++ resources/js/components/_Shared/Moment/DateTimeComponent.vue
@@ -13,19 +13,26 @@
             return {
                 active: 1,
                 display: this.date,
-                type1: this.date,
-                type2: this.date,
-                type3: this.date,
+                type1: null,
+                type2: null,
+                type3: null,
+                type4: null,
+                type5: null,
             }
         },
         mounted() {
             this.type1 = this.date;
-            this.type2 =
-            this.display = Moment(this.date).format('LL');
-            this.type3 = Moment(this.date).fromNow();
+            this.type2 = Moment(this.date).format('L');
+            this.type3 = Moment(this.date).format('LL');
+            this.type4 = Moment(this.date).format('LLL');
+            this.type5 = Moment(this.date).format('LLLL');
+            this.display = this.type5;
         },
         methods: {
             toggle() {
+                /**
+                 * @todo solve it on an elegant way
+                 */
                 switch (this.active) {
                     case 1:
                         this.display = this.type1;
@@ -37,7 +44,19 @@
                         break;
                     case 3:
                         this.display = this.type3;
+                        this.active++;
+                        break;
+                    case 4:
+                        this.display = this.type5;
+                        this.active++;
+                        break;
+                    case 5:
+                        this.display = this.type5;
+                        this.active++;
+                        break;
+                    default:
                         this.active = 1;
+                        this.display = this.type1;
                         break;
                 }
             }
