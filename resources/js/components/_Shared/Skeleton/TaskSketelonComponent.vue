1d0865a86362b77844078d3ce0aa262678689f08
Burak <burakalbayrak@live.com>
2020-01-11 14:24:00 +0300

    sketelon updated regarding to new task navbar

--- resources/js/components/_Shared/Skeleton/TaskSketelonComponent.vue
+++ resources/js/components/_Shared/Skeleton/TaskSketelonComponent.vue
@@ -1,20 +1,14 @@
 <template>
-    <skeleton class="mb-3" theme="opacity" style="width:250px" shape="radius" bg-color="#dcdcdc">
+    <skeleton class="mb-3" theme="opacity" style="width:100%" shape="radius" bg-color="#dcdcdc">
         <row>
-            <i-col :span="6">
+            <i-col :span="1">
                 <tb-skeleton :aspect-ratio="1" shape="circle" class="ml-3">
                 </tb-skeleton>
             </i-col>
             <i-col :span="16">
                 <row>
-                    <i-col :span="12" class="ml-4 mt-1" duration=".6">
-                        <tb-skeleton :aspect-ratio="0.2"></tb-skeleton>
-                    </i-col>
-                    <i-col :span="20" class="ml-4 mt-1" duration=".6">
-                        <tb-skeleton :aspect-ratio="0.1"></tb-skeleton>
-                    </i-col>
-                    <i-col :span="20" class="ml-4 mt-1" duration=".6">
-                        <tb-skeleton :aspect-ratio="0.1"></tb-skeleton>
+                    <i-col :span="16" class="ml-4" duration=".6">
+                        <tb-skeleton :aspect-ratio=".1"></tb-skeleton>
                     </i-col>
                 </row>
             </i-col>
