460f7631620f177a563645901d8dee9fba648cb0
Burak <burakalbayrak@live.com>
2020-01-18 13:12:58 +0000

    Apply fixes from StyleCI

--- app/Interfaces/AssigneeServiceInterface.php
+++ app/Interfaces/AssigneeServiceInterface.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Interfaces;
 
 use App\Models\Assignee;
--- app/Services/AssigneeService.php
+++ app/Services/AssigneeService.php
@@ -11,19 +11,19 @@ use Carbon\Carbon;
 class AssigneeService implements AssigneeServiceInterface
 {
     /**
-     * @var AssigneeRepository $repository
+     * @var AssigneeRepository
      */
     public $repository;
 
     /**
-     * @var TaskServiceInterface $taskService
+     * @var TaskServiceInterface
      */
     public $taskService;
 
     /**
      * Validation base rules.
      *
-     * @var array $rules
+     * @var array
      */
     protected $rules = [
         'due'            => 'sometimes|nullable|date_format:Y-m-d H:i:s',
@@ -114,7 +114,7 @@ class AssigneeService implements AssigneeServiceInterface
     public function complete(string $assignee): Assignee
     {
         $complete = $this->repository->update([
-            'completed_at' => Carbon::now()
+            'completed_at' => Carbon::now(),
         ], $assignee);
 
         $task = $this->taskService->complete($complete->task_id, $complete->id);
@@ -134,7 +134,7 @@ class AssigneeService implements AssigneeServiceInterface
     public function incomplete(string $assignee): Assignee
     {
         $incomplete = $this->repository->update([
-            'completed_at' => null
+            'completed_at' => null,
         ], $assignee);
 
         $task = $this->taskService->incomplete($incomplete->task_id);
