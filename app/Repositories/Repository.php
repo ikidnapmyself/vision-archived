c02c5e04eaf3e160e641ecb754dd7d82799ed23a
Burak <burakalbayrak@live.com>
2020-01-19 09:46:07 +0000

    Apply fixes from StyleCI

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -1,9 +1,10 @@
 <?php
+
 namespace App\Repositories;
 
 use Illuminate\Database\Eloquent\Model;
-use Prettus\Repository\Eloquent\BaseRepository;
 use Prettus\Repository\Contracts\CacheableInterface;
+use Prettus\Repository\Eloquent\BaseRepository;
 use Prettus\Repository\Traits\CacheableRepository;
 
 class Repository extends BaseRepository implements CacheableInterface
@@ -11,7 +12,7 @@ class Repository extends BaseRepository implements CacheableInterface
     use CacheableRepository;
 
     /**
-     * Specify Model class name
+     * Specify Model class name.
      *
      * @return string
      */
