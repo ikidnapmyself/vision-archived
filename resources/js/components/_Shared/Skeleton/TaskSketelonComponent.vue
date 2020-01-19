81369171b959d5d15e5bfe3b9756b3dcb3d2392a
Burak <burakalbayrak@live.com>
2020-01-19 10:27:10 +0300

    task sketelon updated regarding to task components

--- resources/js/components/_Shared/Skeleton/TaskSketelonComponent.vue
+++ resources/js/components/_Shared/Skeleton/TaskSketelonComponent.vue
@@ -2,18 +2,20 @@
     <skeleton class="mb-3" theme="opacity" style="width:100%" shape="radius" bg-color="#dcdcdc">
         <row>
             <i-col :span="1">
-                <tb-skeleton :aspect-ratio="1" shape="circle" class="ml-3">
+                <tb-skeleton :aspect-ratio="1" duration="1">
                 </tb-skeleton>
             </i-col>
-            <i-col :span="16">
-                <row>
-                    <i-col :span="3" class="ml-4 mt-2" duration=".6">
-                        <tb-skeleton :aspect-ratio=".265"></tb-skeleton>
-                    </i-col>
-                    <i-col :span="16" class="ml-4 mt-2" duration=".6">
-                        <tb-skeleton :aspect-ratio=".05"></tb-skeleton>
-                    </i-col>
-                </row>
+            <i-col :span="10" class="ml-4">
+                <tb-skeleton :aspect-ratio=".1" duration="1">
+                </tb-skeleton>
+            </i-col>
+            <i-col :span="1" class="ml-4">
+                <tb-skeleton :aspect-ratio="1" duration="1">
+                </tb-skeleton>
+            </i-col>
+            <i-col :span="1" class="ml-4">
+                <tb-skeleton :aspect-ratio="1" duration="1">
+                </tb-skeleton>
             </i-col>
         </row>
     </skeleton>
