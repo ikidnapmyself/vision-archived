6f205d407fe1db9f9a1ba2a1b0726f4a9481c3a8
Burak <burakalbayrak@live.com>
2020-01-19 11:32:00 +0300

    Status changes centralized (complete, incomplete)

---
+++ app/Http/Requests/TaskStatusRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Interfaces\TaskServiceInterface;
+use App\Models\Status;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class TaskStatusRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @param TaskServiceInterface $service
+     * @return array
+     */
+    public function rules(TaskServiceInterface $service)
+    {
+        $task_id   = $this->route('task');
+        $task      = $service->show($task_id);
+        $completed = Status::COMPLETED;
+        $allowed   = implode(',', $task->availableStatuses());
+
+        return [
+            'status'     => "required|in:{$allowed}",
+            'reason'     => [
+                Rule::requiredIf($task->status()->name === $completed),
+                'max:100'
+            ],
+            'assignee'   => "required_if:status,{$completed}|exists:App\Models\Assignees,id,task_id,{$task_id}",
+        ];
+    }
+}
