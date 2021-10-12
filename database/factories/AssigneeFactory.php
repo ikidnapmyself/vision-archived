56301bb286e18e55fcf847a5089183a8b6b1badf
Burak <ikidnapmyself@users.noreply.github.com>
2021-10-12 16:50:51 +0000

    Apply fixes from StyleCI

--- database/factories/AssigneeFactory.php
+++ database/factories/AssigneeFactory.php
@@ -3,16 +3,16 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Models\Assignee;
-use App\Models\User;
 use App\Models\Task;
+use App\Models\User;
 use Faker\Generator as Faker;
 
 $factory->define(Assignee::class, function (Faker $faker) {
     /**
      * @todo Users can only assign themselves until we use teams actively
      */
-    $user    = User::inRandomOrder()->first();
-    $task    = Task::inRandomOrder()->first();
+    $user = User::inRandomOrder()->first();
+    $task = Task::inRandomOrder()->first();
     /**
      * @todo Generate due and defer dates regarding to project limitations.
      */
@@ -33,7 +33,7 @@ $factory->define(Assignee::class, function (Faker $faker) {
         'defer'          => $defer ?? null,
         'difficulty'     => $faker->boolean ? rand(-10, 10) : null,
         'estimated_time' => rand(15, 15 * 1000),
-        'blocker'        => ! (boolean) rand(0, 5),
+        'blocker'        => ! (bool) rand(0, 5),
         'completed_at'   => null,
     ];
 });
